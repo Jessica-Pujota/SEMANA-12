a58884302e6f0efc8dfb214b3b210e0fe7bb2ce9
Jessica Alexandra Pujota <ja.pujotap@uea.edu.ec>
2025-09-04 14:44:16 -0500

    Actualización de temperaturas

--- TEMPERATURA.py
+++ TEMPERATURA.py
@@ -63,60 +63,60 @@ temperaturas = [
             {"day": "Domingo", "temp": 41}
         ],
         [   # Semana 3
-            {"day": "Lunes", "temp": 61},
-            {"day": "Martes", "temp": 65},
-            {"day": "Miércoles", "temp": 68},
-            {"day": "Jueves", "temp": 70},
-            {"day": "Viernes", "temp": 72},
-            {"day": "Sábado", "temp": 76},
-            {"day": "Domingo", "temp": 80}
+            {"day": "Lunes", "temp": 41},
+            {"day": "Martes", "temp": 45},
+            {"day": "Miércoles", "temp": 48},
+            {"day": "Jueves", "temp": 40},
+            {"day": "Viernes", "temp": 42},
+            {"day": "Sábado", "temp": 46},
+            {"day": "Domingo", "temp": 40}
         ],
         [   # Semana 4
-            {"day": "Lunes", "temp": 64},
-            {"day": "Martes", "temp": 67},
-            {"day": "Miércoles", "temp": 69},
-            {"day": "Jueves", "temp": 71},
-            {"day": "Viernes", "temp": 74},
-            {"day": "Sábado", "temp": 77},
-            {"day": "Domingo", "temp": 80}
+            {"day": "Lunes", "temp":44},
+            {"day": "Martes", "temp": 47},
+            {"day": "Miércoles", "temp": 49},
+            {"day": "Jueves", "temp": 41},
+            {"day": "Viernes", "temp": 44},
+            {"day": "Sábado", "temp": 47},
+            {"day": "Domingo", "temp": 40}
         ]
     ],
     [   # Ciudad 3 "Ayora"
         [   # Semana 1
-            {"day": "Lunes", "temp": 90},
-            {"day": "Martes", "temp": 92},
-            {"day": "Miércoles", "temp": 94},
-            {"day": "Jueves", "temp": 91},
-            {"day": "Viernes", "temp": 88},
-            {"day": "Sábado", "temp": 85},
-            {"day": "Domingo", "temp": 82}
+            {"day": "Lunes", "temp": 36},
+            {"day": "Martes", "temp": 35},
+            {"day": "Miércoles", "temp": 40},
+            {"day": "Jueves", "temp": 39},
+            {"day": "Viernes", "temp": 38},
+            {"day": "Sábado", "temp": 35},
+            {"day": "Domingo", "temp": 32}
         ],
         [   # Semana 2
-            {"day": "Lunes", "temp": 89},
-            {"day": "Martes", "temp": 91},
-            {"day": "Miércoles", "temp": 93},
-            {"day": "Jueves", "temp": 90},
-            {"day": "Viernes", "temp": 87},
-            {"day": "Sábado", "temp": 84},
-            {"day": "Domingo", "temp": 81}
+            {"day": "Lunes", "temp": 39},
+            {"day": "Martes", "temp": 31},
+            {"day": "Miércoles", "temp": 33},
+            {"day": "Jueves", "temp": 40},
+            {"day": "Viernes", "temp": 47},
+            {"day": "Sábado", "temp": 44},
+            {"day": "Domingo", "temp": 41}
         ],
         [   # Semana 3
-            {"day": "Lunes", "temp": 91},
-            {"day": "Martes", "temp": 93},
-            {"day": "Miércoles", "temp": 95},
-            {"day": "Jueves", "temp": 92},
-            {"day": "Viernes", "temp": 89},
-            {"day": "Sábado", "temp": 86},
-            {"day": "Domingo", "temp": 83}
+            {"day": "Lunes", "temp": 34},
+            {"day": "Martes", "temp": 33},
+            {"day": "Miércoles", "temp": 35},
+            {"day": "Jueves", "temp": 32},
+            {"day": "Viernes", "temp": 39},
+            {"day": "Sábado", "temp": 36},
+            {"day": "Domingo", "temp": 33}
         ],
         [   # Semana 4
-            {"day": "Lunes", "temp": 88},
-            {"day": "Martes", "temp": 90},
-            {"day": "Miércoles", "temp": 92},
-            {"day": "Jueves", "temp": 89},
-            {"day": "Viernes", "temp": 86},
-            {"day": "Sábado", "temp": 83},
-            {"day": "Domingo", "temp": 80}
+            {"day": "Lunes", "temp": 38},
+            {"day": "Martes", "temp": 40},
+            {"day": "Miércoles", "temp": 32},
+            {"day": "Jueves", "temp": 39},
+            {"day": "Viernes", "temp": 41},
+            {"day": "Sábado", "temp": 40},
+            {"day": "Domingo", "temp": 39}
         ]
     ]
 ]
